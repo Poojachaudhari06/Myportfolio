61216f28e3d7df2603d43c6fa6868119eb2dcc9b
Pooja <poojavchaudhari06@gmail.com>
2024-03-24 15:41:02 +0530

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <Navbar/>
     <Themes/>
     <Routes>
-      <Route  index element={<Home/>}/>
+      <Route  index element= {<Home/>}/>
       <Route path='about' element={<About/>}/>
       <Route path='portfolio' element={<Portfolio/>}/>
       <Route path='contact' element={<Contact/>}/>
