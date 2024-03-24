1d6bd763b271e7caf703f7823c7f99f0689ba0f6
Pooja <poojavchaudhari06@gmail.com>
2024-03-24 10:00:38 +0530

    new resume

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Info from '../../components/Info';
 import { FaDownload } from 'react-icons/fa';
-import CV from '../../assets/pooja-CV.pdf';
+import CV from '../../assets/Poojavc_23.pdf';
 import Stats from '../../components/Stats';
 import Skills from '../../components/Skills';
 import { resume } from '../../data';
